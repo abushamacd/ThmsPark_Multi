7057ca4305afdd551121b7aead0f4e830153dddc
abushamacd <assiddik001@gmail.com>
2026-07-02 23:03:34 +0600

    off theme switcher function

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,30 @@
 jQuery(document).ready(function () {
   $(document).ready(function () {
-    // Theme Switcher
-    const $checkbox = $("#themeToggle");
-    const $htmlTag = $("html");
+    // // Theme Switcher
+    // const $checkbox = $("#themeToggle");
+    // const $htmlTag = $("html");
 
-    // Load theme from localStorage
-    const savedTheme = localStorage.getItem("theme");
+    // // Load theme from localStorage
+    // const savedTheme = localStorage.getItem("theme");
 
-    if (savedTheme === "light") {
-      $htmlTag.addClass("light").removeClass("dark");
-      $checkbox.prop("checked", true);
-    } else {
-      $htmlTag.addClass("dark").removeClass("light"); // default
-      $checkbox.prop("checked", false);
-    }
+    // if (savedTheme === "light") {
+    //   $htmlTag.addClass("light").removeClass("dark");
+    //   $checkbox.prop("checked", true);
+    // } else {
+    //   $htmlTag.addClass("dark").removeClass("light"); // default
+    //   $checkbox.prop("checked", false);
+    // }
 
-    // Listen for changes
-    $checkbox.on("change", function () {
-      if ($checkbox.is(":checked")) {
-        $htmlTag.removeClass("dark").addClass("light");
-        localStorage.setItem("theme", "light");
-      } else {
-        $htmlTag.removeClass("light").addClass("dark");
-        localStorage.setItem("theme", "dark");
-      }
-    });
+    // // Listen for changes
+    // $checkbox.on("change", function () {
+    //   if ($checkbox.is(":checked")) {
+    //     $htmlTag.removeClass("dark").addClass("light");
+    //     localStorage.setItem("theme", "light");
+    //   } else {
+    //     $htmlTag.removeClass("light").addClass("dark");
+    //     localStorage.setItem("theme", "dark");
+    //   }
+    // });
     // Sticy
     $(window).on("scroll", function () {
       if ($(this).scrollTop() > 50) {
